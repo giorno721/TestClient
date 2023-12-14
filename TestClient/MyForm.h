d783a23390bf4cbf8ef8b4ad827f417bb479a6f4
Roman <roman.marushko.pz.2022@lpnu.ua>
2023-12-14 17:30:58 +0200

    Connection added

--- TestClient/MyForm.h
+++ TestClient/MyForm.h
@@ -1,4 +1,9 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <msclr/marshal_cppstd.h>
+#pragma comment(lib, "ws2_32.lib")
 
 namespace TestClient {
 
@@ -12,6 +17,10 @@ namespace TestClient {
 	/// <summary>
 	/// Summary for MyForm
 	/// </summary>
+
+	const int PORT = 12345;  // Use the same port as the server
+	//const char* SERVER_IP = NULL;  // Replace with the server's IP address
+
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
@@ -92,7 +101,7 @@ namespace TestClient {
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(225, 26);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(21, 15);
+			this->label1->Size = System::Drawing::Size(20, 13);
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"IP:";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
@@ -145,18 +154,120 @@ namespace TestClient {
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		void client()
+		{
+			
+
+			//while (true) {
+			//	// Send a directory path to the server
+			//	std::string directoryPath;
+			//	std::getline(std::cin, directoryPath);//додати зчитування з txtbx
+
+			//	std::string fileExtension;
+			//	std::getline(std::cin, fileExtension);//додати зчитування з cmbbx
+
+			//	std::string fileAndPath = directoryPath + "|" + fileExtension;
+
+			//	if (send(clientSocket, fileAndPath.c_str(),
+			//		static_cast<int>(fileAndPath.size()) + 1, 0) != SOCKET_ERROR) {
+			//		std::cout << "Directory path and file extension sent to the server: " << fileAndPath << std::endl;
+			//	}
+			//	else {
+			//		std::cerr << "Error sending to server. Error code: " << WSAGetLastError() << std::endl;
+			//		break;
+			//	}
+
+			//	// Receive the directory contents from the server
+			//	char buffer[65536];
+			//	int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+			//	if (bytesRead > 0) {
+			//		buffer[bytesRead] = '\0';
+			//		std::cout << "Received directory contents from the server:\n" << buffer << std::endl;
+			//	}
+			//	else if (bytesRead == 0) {
+			//		std::cerr << "Server disconnected.\n";
+			//	}
+			//	else {
+			//		std::cerr << "Error receiving from server. Error code: " << WSAGetLastError() << std::endl;
+			//		break;
+			//	}
+			//}
+
+			//// Cleanup
+			//closesocket(clientSocket);
+			//WSACleanup();
+		}
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		WSADATA wsaData;
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+			directory_contents_textbox->Text = L"WSAStartup failed.";
+			return;
+		}
+
+		SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (clientSocket == INVALID_SOCKET) {
+			directory_contents_textbox->Text = L"Error at socket(): " + WSAGetLastError();
+			WSACleanup();
+			return;
+		}
+
+		sockaddr_in serverAddr;
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_port = htons(PORT);
+		System::String^ SERVER_IP_FORMS = IP_to_conect_textbox->Text;
+		std::string serverIpStdString = msclr::interop::marshal_as<std::string>(SERVER_IP_FORMS);
+		const char* SERVER_IP = serverIpStdString.c_str();
+		inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+
+		if (connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+			//::MessageBox(nullptr, L"Failed to connect to server.", L"Error", MB_OK | MB_ICONERROR);
+			closesocket(clientSocket);
+			WSACleanup();
+			return;
+		}
+
+		directory_contents_textbox->Text += "Connected to the server." + "\n";
 	}
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) 
+{   
+	file_type_comboBox->Items->Add("*.all");
+	file_type_comboBox->Items->Add("*.txt");
+	file_type_comboBox->Items->Add("*.doc");
+	file_type_comboBox->Items->Add("*.docx");
+	file_type_comboBox->Items->Add("*.pdf");
+	file_type_comboBox->Items->Add("*.ppt");
+	file_type_comboBox->Items->Add("*.pptx");
+	file_type_comboBox->Items->Add("*.jpg");
+	file_type_comboBox->Items->Add("*.png");
+	file_type_comboBox->Items->Add("*.gif");
+	file_type_comboBox->Items->Add("*.mp3");
+	file_type_comboBox->Items->Add("*.mp4");
+	file_type_comboBox->Items->Add("*.avi");
+	file_type_comboBox->Items->Add("*.swf");
+	file_type_comboBox->Items->Add("*.rar");
+	file_type_comboBox->Items->Add("*.zip");
+	file_type_comboBox->Items->Add("*.iso");
+	file_type_comboBox->Items->Add("*.img");
+	file_type_comboBox->Items->Add("*.exe");
+	file_type_comboBox->Items->Add("*.dll");
+	file_type_comboBox->Items->Add("*.bat");
+	file_type_comboBox->Items->Add("*.cmd");
+	file_type_comboBox->Items->Add("*.com");
+	file_type_comboBox->Items->Add("*.msi");
 }
 };
 }
