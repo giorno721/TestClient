9efbf03aa58228530c613540c9d38c7497fd29b4
Roman <roman.marushko.pz.2022@lpnu.ua>
2023-12-14 19:31:26 +0200

    Print should be fixed

--- TestClient/MyForm.h
+++ TestClient/MyForm.h
@@ -20,6 +20,7 @@ namespace TestClient {
 
 	const int PORT = 12345;  // Use the same port as the server
 	//const char* SERVER_IP = NULL;  // Replace with the server's IP address
+	SOCKET clientSocket;
 
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -212,7 +213,7 @@ namespace TestClient {
 			return;
 		}
 
-		SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 		if (clientSocket == INVALID_SOCKET) {
 			directory_contents_textbox->Text = L"Error at socket(): " + WSAGetLastError();
 			WSACleanup();
@@ -228,24 +229,57 @@ namespace TestClient {
 		inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
 
 		if (connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
-			//::MessageBox(nullptr, L"Failed to connect to server.", L"Error", MB_OK | MB_ICONERROR);
+			directory_contents_textbox->Text = L"Failed to connect to server: " + WSAGetLastError();
 			closesocket(clientSocket);
 			WSACleanup();
 			return;
 		}
 
-		directory_contents_textbox->Text += "Connected to the server." + "\n";
+		directory_contents_textbox->Text += "Connected to the server.";
 	}
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		std::string directoryPath;
+		System::String^ directoryPathForms = directory_path_textbox->Text;
+		directoryPath = msclr::interop::marshal_as<std::string>(directoryPathForms);
+		std::string fileExtension;
+		System::String^ fileExtensionForms = file_type_comboBox->Text;
+		fileExtension = msclr::interop::marshal_as<std::string>(fileExtensionForms);
+		std::string fileAndPath = directoryPath + "|" + fileExtension;
+		if (send(clientSocket, fileAndPath.c_str(),
+			static_cast<int>(fileAndPath.size()) + 1, 0) != SOCKET_ERROR) {
+			directory_contents_textbox->Text += Environment::NewLine + "Directory path and file extension sent to the server: " + Environment::NewLine + msclr::interop::marshal_as<System::String^>(fileAndPath);
+
+		}
+		else {
+			directory_contents_textbox->Text += Environment::NewLine + "Error sending to server. Error code: " + WSAGetLastError() + "\n";
+		}
+		// Receive the directory contents from the server
+		char buffer[65536];
+		int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+		if (bytesRead > 0) {
+			buffer[bytesRead] = '\0';
+			directory_contents_textbox->Text += Environment::NewLine + "Received directory contents from the server:\n" + Environment::NewLine + msclr::interop::marshal_as<System::String^>(buffer);	
+		}
+		else if (bytesRead == 0) {
+			directory_contents_textbox->Text += Environment::NewLine + "Server disconnected.";
+		}
+		else {
+			directory_contents_textbox->Text += Environment::NewLine + "Error receiving from server. Error code: " + WSAGetLastError();
+		}
+		//// Cleanup
+		//closesocket(clientSocket);
+		//WSACleanup();
+	}
+
 private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) 
 {   
 	file_type_comboBox->Items->Add("*.all");
-	file_type_comboBox->Items->Add("*.txt");
+	file_type_comboBox->Items->Add(".txt");
 	file_type_comboBox->Items->Add("*.doc");
 	file_type_comboBox->Items->Add("*.docx");
 	file_type_comboBox->Items->Add("*.pdf");
